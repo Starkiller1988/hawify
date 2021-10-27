22770fd7bc9afc2d66eb5b0e8cb74b16a36761c2
Samir Schabel <schabel.samir@gmail.com>
2021-10-27 22:27:47 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Footer from '../header&footer/footer';
+import Style from '../src/App.css';
 
 
 function App() {
@@ -7,6 +9,7 @@ function App() {
   return (
     <div>
       <h1>Hello World!</h1>
+<Footer />
     </div>
   )
 }

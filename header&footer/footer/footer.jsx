cc32bc7a5a7397493f94d8984b411cd6b231ba85
Samir Schabel <schabel.samir@gmail.com>
2021-10-28 23:31:51 +0200

    changes

---
+++ header&footer/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../footer/footer.css';
+
+const Footer = () => (
+    <div className="footer">
+    <footer>
+        <img src="" alt="" />
+    </footer>
+    </div>
+);
+
+export default Footer;
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react'
-import Footer from '../header&footer/footer';
-import Style from '../src/App.css';
+import Footer from '../header&footer/footer/footer'
+import styles from '../src/App.css'
 
 
 function App() {
 
 
   return (
-    <div>
-      <h1>Hello World!</h1>
-<Footer />
-    </div>
+
+<>
+<div className= "buttons">
+  <button className= "cashbook"><p>Kassenbuch</p></button>
+  <button className= "shopping"><p>Shopping-List</p></button>
+  <button className="calculator"><p>Taschenrechner</p></button>
+  <button className="calendar"><p>Kalender</p></button>
+  <button className="aboutapp"><p>Über HAWI.FY</p></button>
+</div>
+
+    <Footer />
+    </>
   )
 }
 

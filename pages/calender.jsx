9f9813b78cf232416e3ae4c3b789cb2b51a2c880
Samir Schabel <schabel.samir@gmail.com>
2021-10-31 22:43:04 +0100

    changes

--- pages/calender.jsx
+++ pages/calender.jsx
@@ -7,6 +7,24 @@ const ToDoList = () => {
         Startseite
       </Link>
 
+      <div>
+        <form className="choose_day">
+          <select>
+            <option value="Montag">Montag</option>
+            <option value="Dienstag">Dienstag</option>
+            <option value="Mittwoch">Mittwoch</option>
+            <option value="Donnerstag">Donnerstag</option>
+            <option value="Freitag">Freitag</option>
+            <option value="Samstag">Samstag</option>
+            <option value="Sonntag">Sonntag</option>
+          </select>
+        </form>
+      </div>
+
+<div className="todo_field">
+<h2>To-Do Liste</h2>
+</div>
+
       <input
         type="form"
         placeholder="Hier eingeben..."

f1a7dd2e650322cb25620106573a61ee55f5bfd4
Samir Schabel <schabel.samir@gmail.com>
2021-10-31 12:25:03 +0100

    changes

--- pages/calender.jsx
+++ pages/calender.jsx
@@ -1,20 +1,24 @@
 import { Link } from "react-router-dom";
 
-
 const ToDoList = () => {
-    return(
-        <div className="todo">
+  return (
+    <div className="todo">
+      <Link to="/" className="start">
+        Startseite
+      </Link>
 
-<Link to="/" className="start">Home</Link>
+      <input
+        type="form"
+        placeholder="Hier eingeben..."
+        className="input_todos"
+      />
 
-
-
-
-
-
-
-        </div>
-    )
-}
+      <div className="delete_clear">
+        <button>Aufgabe entfernen</button>
+        <button>Liste löschen</button>
+      </div>
+    </div>
+  );
+};
 
 export default ToDoList;

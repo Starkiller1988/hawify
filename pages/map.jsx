1bf6603dd0ae048b27136509efb86eb42e460e25
Samir Schabel <schabel.samir@gmail.com>
2021-11-03 15:20:00 +0100

    add map component

---
+++ pages/map.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
+import { Link } from 'react-router-dom';
+
+export default function map() {
+
+    return (
+<div>
+<Link to="/" className="start">
+        Startseite
+      </Link>
+
+</div>
+    )
+}

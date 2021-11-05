68f16754215e392fdafd3c690c4916a19747e589
Samir Schabel <schabel.samir@gmail.com>
2021-11-05 13:36:12 +0100

    renew my shopping component

--- components/shopping_components/Basket.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Basket(props) {
-    return <aside className="block .col-1">
-        <h2>Korbinhalt</h2>
-    </aside>
-}
--- pages/shoppingList.jsx
+++ pages/shoppingList.jsx
@@ -1,25 +1,13 @@
 import { Link } from "react-router-dom";
-import HeaderShop from '../Header_Footer_Components/header_shoppingList/header_shop'
-import Main from '../components/shopping_components/Main'
-import Basket from '../components/shopping_components/Basket'
-import '../components/shopping_components/shoppingList.css';
-import data from '../components/shopping_components/Data'
 
-const ShoppingList = () => {
+function ShoppingList() {
 
-  const { products } = data;
   return (
     <div className="shop">
       <Link to="/" className="start">
         Startseite
       </Link>
 
-      <HeaderShop></HeaderShop>
-      <div className="row">
-        <Main products={products}></Main>
-        <Basket></Basket>
-      </div>
-      
       
 
 

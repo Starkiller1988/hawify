6158489fb03ab995ef97efbe29c4380415d21342
Samir Schabel <90188949+Starkiller1988@users.noreply.github.com>
2021-11-11 22:57:21 +0100

    Update header&footer/header/header.jsx
    Co-authored-by: Lene <51355141+macfrei@users.noreply.github.com>

--- header&footer/header/header.jsx
+++ header&footer/header/header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import styles from "../header/header.css";
 
 const Header = () => (
-  <div className="header">
+  <header className="header">
     <p>Menu</p>
-  </div>
+  </header>
 );
 
 export default Header;
